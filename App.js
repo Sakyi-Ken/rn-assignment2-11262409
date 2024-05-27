35cbb9e04d18fa2c152cc088bdce3bf15460da7d
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2024-05-27 11:55:09 +0000

    changed the text in the app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.name}>My name is Emmanuel Sakyi</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
